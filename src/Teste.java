b48ba0b916540a38c43bf535f29fbff0fe3324ce
Mthws167 <mthws.henrique@hotmail.com>
2023-03-15 22:32:42 -0300

    correções e validações

--- src/Teste.java
+++ src/Teste.java
@@ -6,56 +6,61 @@ public class Teste {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		System.out.print("Digite a quantidade de números a serem ordenados: ");
-		int quantidadeNumeros = input.nextInt();
-		if (quantidadeNumeros > 0) {
+		try {
+			System.out.print("Digite a quantidade de números a serem ordenados: ");
+			int quantidadeNumeros = input.nextInt();
+			if (quantidadeNumeros > 0) {
 
-			int[] numeros = new int[quantidadeNumeros];
+				int[] numeros = new int[quantidadeNumeros];
 
-			for (int i = 0; i < quantidadeNumeros; i++) {
-				System.out.print("Digite o número #" + (i + 1) + ": ");
-				try {
-					numeros[i] = input.nextInt();
-				} catch (InputMismatchException e) {
-					System.out.println("Conteúdo inválido do arquivo.");
+				for (int i = 0; i < quantidadeNumeros; i++) {
+					System.out.print("Digite o número #" + (i + 1) + ": ");
+					try {
+						numeros[i] = input.nextInt();
+					} catch (InputMismatchException e) {
+						System.out.println("Conteúdo inválido do arquivo.");
+						return;
+					}
+					if (numeros[i] > 0 && numeros[i] < 65530) {
+						continue;
+					} else {
+						System.out.print("Tamanho inválido do vetor.");
+						return;
+					}
+				}
+
+				if (numeros.length != quantidadeNumeros) {
+					System.out.println("Número de elementos divergente do informado.");
 					return;
 				}
-				if (numeros[i] > 0 && numeros[i] < 65530) {
-					continue;
-				} else {
-					System.out.print("Tamanho inválido do vetor.");
-					return;
-				}
-			}
-			
-			if (numeros.length != quantidadeNumeros) {
-	            System.out.println("Número de elementos divergente do informado.");
-	            return;
-	        }
-			
-			System.out.print("\n\n");
-			System.out.print("Números desordenados:  \n");
-			for (int i = 0; i < quantidadeNumeros; i++) {
-				if (i == quantidadeNumeros - 1) {
-					System.out.print(numeros[i] + ".");
-				} else {
-					System.out.print(numeros[i] + ", ");
-				}
-			}
 
-			System.out.print("\n\n");
-			Arrays.sort(numeros);
-			System.out.print("Números ordenados:  \n");
-			for (int i = 0; i < quantidadeNumeros; i++) {
-				if (i == quantidadeNumeros - 1) {
-					System.out.print(numeros[i] + ".");
-				} else {
-					System.out.print(numeros[i] + ", ");
+				System.out.print("\n\n");
+				System.out.print("Números desordenados:  \n");
+				for (int i = 0; i < quantidadeNumeros; i++) {
+					if (i == quantidadeNumeros - 1) {
+						System.out.print(numeros[i] + ".");
+					} else {
+						System.out.print(numeros[i] + ", ");
+					}
 				}
-			}
 
-		} else {
-			System.out.print("Quantidade inválida!");
+				System.out.print("\n\n");
+				Arrays.sort(numeros);
+				System.out.print("Números ordenados:  \n");
+				for (int i = 0; i < quantidadeNumeros; i++) {
+					if (i == quantidadeNumeros - 1) {
+						System.out.print(numeros[i] + ".");
+					} else {
+						System.out.print(numeros[i] + ", ");
+					}
+				}
+
+			} else {
+				System.out.print("Quantidade inválida!");
+				return;
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Conteúdo inválido do arquivo.");
 			return;
 		}
 
